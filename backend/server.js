a6600a5dae9c9b666f8fe459415c3e563dbcc5b3
Atharv-66 <atharvpandey112@gmail.com>
2025-04-19 15:53:38 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 const express = require("express");
-const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
-
 const { notFound, errorHandler } = require("./middlewares/errorMiddleware");
 const path = require("path");
 
@@ -13,6 +11,8 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
+
+// Use the userRoutes for the /api/user path
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
@@ -50,12 +50,14 @@ const server = app.listen(
   PORT,
   console.log(`Server started on port ${PORT}`.yellow.bold)
 );
+
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
     origin: "http://localhost:3000",
   },
 });
+
 io.on("connection", (socket) => {
   console.log("Connected to socket.io");
   socket.on("setup", (userData) => {
@@ -67,17 +69,15 @@ io.on("connection", (socket) => {
     socket.join(room);
     console.log("User Joined Room: " + room);
   });
+
   socket.on("typing", (room) => socket.in(room).emit("typing"));
   socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
   socket.on("new message", (newMessageRecieved) => {
-    var chat = newMessageRecieved.chat;
-
+    const chat = newMessageRecieved.chat;
     if (!chat.users) return console.log("chat.users not defined");
-
     chat.users.forEach((user) => {
-      if (user._id == newMessageRecieved.sender._id) return;
-
+      if (user._id === newMessageRecieved.sender._id) return;
       socket.in(user._id).emit("message recieved", newMessageRecieved);
     });
   });
